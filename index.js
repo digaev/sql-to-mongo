59b522ce0f96b4751cee09512fbb8889d4abac2c
Nikolay Digaev <ffs.cmp@gmail.com>
2017-01-16 22:59:57 +0500

    Code cleanup

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 const fs = require('fs')
 const SqlToMongoTranslator = require('./translator')
+const util = require('util')
 
-const sql = "SELECT * FROM svista.balans WHERE card_no='8600000000000001'"
-const config = JSON.parse(fs.readFileSync('./translator.config.json'))
-const translator = new SqlToMongoTranslator(config)
+const args = process.argv.slice(2)
+if (args.length === 0) {
+  console.log('Usage: node . "SQL QUERY"')
+  console.log('Example: node . "SELECT * FROM table"')
+  process.exit()
+} else {
+  const sql = args.join(' ')
+  const config = JSON.parse(fs.readFileSync('./translator.config.json'))
+  const translator = new SqlToMongoTranslator(config)
 
-translator.query(sql).then((docs) => {
-  console.log(docs)
-}).catch((err) => {
-  console.log(err)
-})
-
-// const args = process.argv.slice(2)
-// if (args.length === 0) {
-//   console.log('Usage: node . "SQL QUERY"')
-//   console.log('Example: node . "SELECT * FROM table"')
-//   process.exit()
-// } else {
-//   // const sql = "SELECT * FROM svista.balans WHERE card_no IN('8600482914550428', '8600000000000001')"
-//   const sql = args.join(' ')
-//   const config = JSON.parse(fs.readFileSync('./translator.config.json'))
-//   const translator = new SqlToMongoTranslator(config)
-
-//   translator.query(sql).then(function (err, result) {
-//     console.log(err)
-//     console.log(result)
-//   });
-// }
+  translator.query(sql).then((docs) => {
+    console.log('Result: ' + util.inspect(docs, false, null))
+    return 0
+  }).catch((err) => {
+    console.log('An error occured :( ' + err.message)
+    return 1
+  }).then(process.exit)
+}
